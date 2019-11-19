1b82406ff1accf143f9627e76f0b41010896d7ee
konstantin21093 <web2109@yandex.ru>
2019-11-19 21:56:23 +0700

    Сверстал финальную версию, сайт готов, есть небольшие недочёты по ссылкам

--- js/script.js
+++ js/script.js
@@ -1,4 +1,23 @@
-burger.onclick = function () {
-  document.getElementById("navbar").classList.toggle('active');
-  this.classList.toggle('active-burg');
+window.onload = function() {
+  //работа мобильного меню
+  burger.onclick = function () {
+    document.getElementById("navbar").classList.toggle('active');
+    this.classList.toggle('active-burg');
+  }
+  // плавный скрол при навигации
+  $(".navbar__menu a").mPageScroll2id(); 
+  // кнопка вверх
+  $('body').append('<button class="btn_up" />');
+  $('.btn_up').click(function() {
+    $('body').animate({'scrollTop': 0}, 1000);
+    $('html').animate({'scrollTop': 0}, 1000);
+  });
+  $(window).scroll(function(){
+    if($(window).scrollTop() > 200) {
+      $('.btn_up').addClass('btn_up-active');
+    }
+    else {
+      $('.btn_up').removeClass('btn_up-active');
+    }
+  });
 }
